15775dfc9068d9d6be9bd04b508c2f4d203aff37
Nambu HariSaran <nambuharisaran123@gmail.com>
2025-08-04 16:19:29 +0530

    update

--- app.py
+++ app.py
@@ -23,21 +23,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit file upload size to
 import firebase_admin
 from firebase_admin import credentials, auth
 if not firebase_admin._apps:
-    firebase_creds = {
-        "type": os.getenv("FIREBASE_TYPE"),
-        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        # Replace escaped newlines with actual newlines for private_key
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n') if os.getenv("FIREBASE_PRIVATE_KEY") else None,
-        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
-    }
-    cred = credentials.Certificate(firebase_creds)
+    cred = credentials.Certificate("aqro-f0322-firebase-adminsdk-fbsvc-f82124232c.json")
     firebase_admin.initialize_app(cred)
 
 # Logger setup
